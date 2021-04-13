84aeb86108d4e5fc2b58e9f3488b759b7d254c8f
Lam <55865874+Lam601@users.noreply.github.com>
2021-04-13 17:07:28 +0700

    fix

--- Online_Order_For_Digital_Printing_Of_Photos/Controllers/HomeController.cs
+++ Online_Order_For_Digital_Printing_Of_Photos/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using Online_Order_For_Digital_Printing_Of_Photos.Common;
-using Online_Order_For_Digital_Printing_Of_Photos.Models.DAO;
-using Online_Order_For_Digital_Printing_Of_Photos.Models.ModelViews;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,79 +10,8 @@ namespace Online_Order_For_Digital_Printing_Of_Photos.Controllers
     {
         public ActionResult Index()
         {
-            var model = new PhotoDAO().PhotoList();
-            return View(model);
-        }
-
-        [HttpGet]
-        public ActionResult Login()
-        {
             return View();
         }
-
-        [HttpPost]
-        public ActionResult Login(LoginModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var dao = new UserDAO();
-                var res = dao.Login(model.userName, model.userPwd);
-                if (res == 1)
-                {
-                    var user = dao.GetUserByUserName(model.userName);
-                    //var stt = dao.GetByIdd(user.userID);
-                    if (user.role == "user")
-                    {
-                        var userSession = new UserSession();
-                        userSession.userID = user.userID;
-                        userSession.userName = user.userName;
-                        userSession.userPwd = user.userPwd;
-                        userSession.role = user.role;
-                        userSession.name = user.name;
-                        Session.Add(CommonConstant.USER_SESSION, userSession);
-                        return RedirectToAction("Index", "Home");
-                    }
-                    else if (user.role == "admin")
-                    {
-                        var userSession = new UserSession();
-                        userSession.userID = user.userID;
-                        userSession.userName = user.userName;
-                        userSession.userPwd = user.userPwd;
-                        userSession.role = user.role;
-                        userSession.name = user.name;
-                        Session.Add(CommonConstant.USER_SESSION, userSession);
-                        return RedirectToAction("", "Admin");
-                    }
-
-                }
-                else if (res == 0)
-                {
-                    ModelState.AddModelError("", "Not Available!");
-                }
-                else if (res == -1)
-                {
-                    ModelState.AddModelError("", "You Are blocking!");
-                }
-                else if (res == -2)
-                {
-                    ModelState.AddModelError("", "passwrod isvalid!");
-                }
-                else
-                {
-                    ModelState.AddModelError("", "Login False!");
-                }
-
-            }
-            return View("Login");
-        }
-
-        //Logout 
-        public ActionResult Logout()
-        {
-            Session[CommonConstant.USER_SESSION] = null;
-            return Redirect("/");
-        }
-
 
         public ActionResult About()
         {
