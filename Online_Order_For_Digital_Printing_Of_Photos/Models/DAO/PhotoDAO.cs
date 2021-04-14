ee28eb6dd3fd975764819c67c55798b1b8d9539c
Lam <55865874+Lam601@users.noreply.github.com>
2021-04-14 11:57:04 +0700

    add for test

---
+++ Online_Order_For_Digital_Printing_Of_Photos/Models/DAO/PhotoDAO.cs
@@ -0,0 +1,32 @@
+﻿using Online_Order_For_Digital_Printing_Of_Photos.Models.Entities;
+using Online_Order_For_Digital_Printing_Of_Photos.Models.ModelViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Online_Order_For_Digital_Printing_Of_Photos.Models.DAO
+{
+    public class PhotoDAO
+    {
+        OnlineOrderEntities db = null;
+        public PhotoDAO()
+        {
+            db = new OnlineOrderEntities();
+        }
+
+        public List<PhotoModel> GetAllPhotos()
+        {
+            var res = db.Photos.Where(x => x.status != 0).Select(x => new PhotoModel
+            {
+                photoID = x.photoID,
+                description = x.description,
+                cateID = x.cateID,
+                photoLink = x.photoLink,
+                photoName = x.photoName,
+                status = x.status
+            }).ToList();
+            return res;
+        }
+    }
+}
--- Online_Order_For_Digital_Printing_Of_Photos/Models/DAO/UserDAO.cs
+++ Online_Order_For_Digital_Printing_Of_Photos/Models/DAO/UserDAO.cs
@@ -7,22 +7,66 @@ using System.Web;
 
 namespace Online_Order_For_Digital_Printing_Of_Photos.Models.DAO
 {
-    
+
     public class UserDAO
     {
         // declare entities
         OnlineOrderEntities entities = null;
-        public UserDAO() {
+        public UserDAO()
+        {
             // new in constructor
             entities = new OnlineOrderEntities();
         }
-         
+
         // show All User
-        public List<UserModelView> ShowAllUser() {
+        public List<UserModelView> ShowAllUser()
+        {
 
-            List<UserModelView> rs = entities.Users.Select(d => new UserModelView { userID = (int)d.userID, userName = d.userName, userPwd = d.userPwd, email = d.email, address = d.address, status = d.status, role = d.role, name = d.name }).ToList();
+            List<UserModelView> res = entities.Users.Select(d => new UserModelView
+            {
+                userID = (int)d.userID,
+                userName = d.userName,
+                userPwd = d.userPwd,
+                email = d.email,
+                address = d.address,
+                status = d.status,
+                role = d.role,
+                name = d.name
+            }).ToList();
 
-            return rs;
+            return res;
+        }
+
+        public Users GetUserByUserName(string username)
+        {
+            return entities.Users.SingleOrDefault(x => x.userName == username);
+        }
+
+        public int Login(string username, string password)
+        {
+            var res = entities.Users.SingleOrDefault(x => x.userName == username);
+            if (res == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (res.status == 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (res.userPwd == password)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2;
+                    }
+                }
+            }
         }
     }
 }
